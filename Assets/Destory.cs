4dfc61e74343877402a71bf15c8566886b8c9f29
池田謙正 <yusya30.0220@gmail.com>
2020-03-03 15:51:47 +0900

    ３D提出課題２

---
+++ Assets/Destory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{//Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start()
+    { //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+      
+    }
+    // Update is called once per frame
+    void Update()
+        
+    {
+       
+        if (unitychan.transform.position.z -5 > this.transform.position.z ) 
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
+
